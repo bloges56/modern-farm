21ea5c1495b6077eedd0b7fcc934e429ec10d859
Brady Logan <brady.drew.logan@gmail.com>
2020-08-21 13:30:43 -0500

    created tractor.js that exports a function that takes a plan and calls addPlant for each plant in the plan

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 import { createPlan } from './plan.js'
-import { createAsparagus } from './seeds/asparagus.js'
-import { createCorn } from './seeds/corn.js'
-import { addPlant, usePlants } from './field.js'
+import { plantSeeds } from './tractor.js'
+import { usePlants } from './field.js'
 
 console.log("Welcome to the main module")
 
@@ -9,16 +8,7 @@ var plan = createPlan();
 
 console.log(plan);
 
-const asparagusSeed = createAsparagus();
-
-console.log(asparagusSeed);
-
-const cornSeed = createCorn();
-
-console.log(cornSeed);
-
-addPlant(asparagusSeed);
-addPlant(cornSeed);
+plantSeeds(plan);
 
 var field = usePlants();
 
---
+++ src/scripts/tractor.js
@@ -0,0 +1,32 @@
+import { createAsparagus } from './seeds/asparagus.js'
+import { createCorn } from './seeds/corn.js'
+import { createPotato } from './seeds/potato.js'
+import { createSoybean } from './seeds/soybean.js'
+import { createSunflower } from './seeds/sunflower.js'
+import { createWheat } from './seeds/wheat.js'
+import { addPlant } from './field.js'
+
+export const plantSeeds = (plan) => {
+    for(var i of plan){
+        for(var j of i){
+            if(j === "Asparagus"){
+                addPlant(createAsparagus());
+            }
+            if(j === "Corn"){
+                addPlant(createCorn());
+            }
+            if(j === "Potato"){
+                addPlant(createPotato());
+            }
+            if(j === "Soybean"){
+                addPlant(createSoybean());
+            }
+            if(j === "Sunflower"){
+                addPlant(createSunflower());
+            }
+            if(j === "Wheat"){
+                addPlant(createWheat());
+            }
+        }
+    }
+};
