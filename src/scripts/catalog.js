5179a7692bf2064e63a057d36a1c11a0bc8764b7
Brady Logan <brady.drew.logan@gmail.com>
2020-08-26 10:08:32 -0500

    updated catalog.js to sort plants alphabetically

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,8 +1,17 @@
 export const Catalog = (food) => {
     var contentElement = document.querySelector(".container");
+
+    var foodArray =[];
+
     for(var plant of food){
+        foodArray.push(plant.type);
+    }
+
+    foodArray.sort();
+
+    for(var plant of foodArray){
         contentElement.innerHTML += `
-            <section class="plant">${plant.type}</section>
+            <section class="plant">${plant}</section>
         `;
     }
 };
